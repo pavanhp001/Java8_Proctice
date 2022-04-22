9315d1ea8dbe11abaf86c89fbba7f7bc70d72139
pavan <pavan.hp001@gmail.com>
2022-04-22 08:33:24 +0530

    added builder pattern

---
+++ src/main/java/subramanion/patterns/builder/v2/Builder.java
@@ -0,0 +1,35 @@
+package subramanion.patterns.builder.v2;
+
+
+class Mailer{
+	public static void print(String msg) {
+		System.out.println(msg);
+	}
+	
+	public void from(String from) {print("from");}
+	public void to(String to) {print("to");}
+	public void subject(String subject) {print("subject");}
+	public void body(String body) {print("body");}
+	public void send() {print("sending");}
+}
+
+
+
+public class Builder { //also known as Cascade method pattern
+
+	public static void main(String[] args) {
+
+		Mailer mailer = new Mailer();
+		mailer.from("pavan.hp001@gmail.com");
+		mailer.to("pavan.scala24@gmail.com");
+		mailer.subject("subject");
+		mailer.body("body");
+		mailer.send();
+	}
+	
+	/* problems with this code
+	1.we keep saying mailer
+	
+	*/
+
+}
