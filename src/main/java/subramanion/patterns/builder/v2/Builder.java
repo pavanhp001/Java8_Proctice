a1b2d544040e9760fe66a0faa9f2327c8342911f
pavan <pavan.hp001@gmail.com>
2022-04-22 19:54:31 +0530

    Updated builder

--- src/main/java/subramanion/patterns/builder/v2/Builder.java
+++ src/main/java/subramanion/patterns/builder/v2/Builder.java
@@ -1,16 +1,20 @@
 package subramanion.patterns.builder.v2;
 
+import java.util.function.Consumer;
 
 class Mailer{
 	public static void print(String msg) {
 		System.out.println(msg);
 	}
 	
-	public void from(String from) {print("from");}
-	public void to(String to) {print("to");}
-	public void subject(String subject) {print("subject");}
-	public void body(String body) {print("body");}
-	public void send() {print("sending");}
+	public Mailer from(String from) {print("from"); return this;}
+	public Mailer to(String to) {print("to"); return this; }
+	public Mailer subject(String subject) {print("subject"); return this;}
+	public Mailer body(String body) {print("body"); return this;}
+	public static void send(Consumer<Mailer> consume) {print("sending");
+		Mailer mailer = new Mailer();//Here we can return or don't return the mailer object
+		consume.accept(mailer);
+	}
 }
 
 
@@ -19,17 +23,16 @@ public class Builder { //also known as Cascade method pattern
 
 	public static void main(String[] args) {
 
-		Mailer mailer = new Mailer();
-		mailer.from("pavan.hp001@gmail.com");
-		mailer.to("pavan.scala24@gmail.com");
-		mailer.subject("subject");
-		mailer.body("body");
-		mailer.send();
+		Mailer.send(mailer -> mailer
+		.from("pavan.hp001@gmail.com")
+		.to("pavan.scala24@gmail.com")
+		.subject("subject")
+		.body("body"));
 	}
 	
 	/* problems with this code
 	1.we keep saying mailer
-	
+	2. how can we reuse the code
 	*/
 
 }
